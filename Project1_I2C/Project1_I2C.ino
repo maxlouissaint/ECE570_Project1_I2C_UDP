1e734e1d07521b7d7994bb01ba6174afa0aac381
maxlouissaint <maxlouissaint@gmail.com>
2024-10-25 00:48:28 -0400

    updated to fully working code with a lot of commented codes. Only kept the files that are related to the project

--- Project1_I2C/Project1_I2C.ino
+++ Project1_I2C/Project1_I2C.ino
@@ -4,7 +4,25 @@ Sensor Spec: https://hiletgo.com/ProductDetail/2157948.html
 Version: 1.0
 Date: 8/27/2023 
 */
-#include<Wire.h> //library for facilitating I2C communication between 2 devices
+#include<Wire.h>  // library for facilitating I2C communication between 2 devices
+#include <WiFi.h> // WiFi library for ESP32
+
+// The ssid and password of the WIFI network the ESP32 connects to.
+const char* ssid = "MOTO7DD2";
+const char* password = "u44aa7k5nx";
+
+/*
+  Configures the UDP communication by specifying the 
+  IP address of the server (your computer)
+*/
+const char * udpAddress = "192.168.0.21"; //the IP address of the computer in local WiFi 
+//found using ifconfig on mac OS under "en0 -> inet ->"
+
+//speficying computer port number
+const int udpPort = 6000;
+WiFiUDP udp; // Create WIFI UDP instance
+
+uint8_t ack;
 
 const uint8_t addr = 0x68; //sensor (slave device) address
 size_t L = 14; //number of bytes to read from sensor
@@ -12,12 +30,29 @@ int16_t xA, yA, zA, T, xG, yG, zG; //store raw data from sensor
 float xAcc, yAcc, zAcc, Temp, xGyr, yGyr, zGyr; //converted sensor data
 
 void setup(){
+  Serial.begin(115200); //initialize serial communication
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  //Setup WIFI
+  WiFi.begin(ssid, password); //start wifi connection
+  while (WiFi.isConnected() != true) { //check connection status. keep checking until connected
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP Address: ");
+  Serial.print(WiFi.localIP());
+  
+   //Setup I2C
   Wire.begin(); //initialize I2C communication
+
+  //Initialize Sensor connection
   Wire.beginTransmission(addr); //start communicating with sensor
   Wire.write(0x6B); // write to power management register to put sensor to active mode
   Wire.write(0); //value 0 is written to the register to disable sleep mode
   Wire.endTransmission(true); //end transmission. true will send a stop message, releasing the bus after transmission
-  Serial.begin(115200); //initialize serial communication
 }
 
 void loop(){
@@ -28,7 +63,7 @@ void loop(){
   
   /*
   The Wire.read() function reads two bytes at a time (high byte and low byte) for each 
-  axis of the accelerometer (xA, yA, zA), the temperature (T), and the gyroscope (xG, yG, zG).
+  axis of the accelerometer (xA, yA, zA), the temperature (T), and the gyroscope (xG, yG, zG) from register 0x3B to 0x48.
   The two bytes are combined using bit-shifting and bitwise OR to form a single int16_t value for each axis
   */
   xA = (Wire.read()<<8|Wire.read());
@@ -51,16 +86,74 @@ void loop(){
   float roll  = atan2(yAcc, zAcc) * 180.0/M_PI;
   float pitch = atan2(-xAcc, sqrt(zAcc * zAcc + yAcc * yAcc)) * 180.0/M_PI;
  
-  Serial.print(xAcc); Serial.print(",");
-  Serial.print(yAcc); Serial.print(",");
-  Serial.print(zAcc); Serial.print(",");
-  Serial.print(xGyr); Serial.print(",");
-  Serial.print(yGyr); Serial.print(",");
-  Serial.print(zGyr); Serial.print(",  ");
-  Serial.print(Temp); Serial.print(", ");
-  Serial.print(roll); Serial.print(",  ");
-  Serial.print(pitch); Serial.print(",  ");
+  // Serial.print(xAcc); Serial.print(",");
+  // Serial.print(yAcc); Serial.print(",");
+  // Serial.print(zAcc); Serial.print(",");
+  // Serial.print(xGyr); Serial.print(",");
+  // Serial.print(yGyr); Serial.print(",");
+  // Serial.print(zGyr); Serial.print(",  ");
+  // Serial.print(Temp); Serial.print(", ");
+  Serial.print("Roll: ");Serial.print(roll); Serial.print(",  ");
+  Serial.print("Pitch: ");Serial.print(pitch);
   Serial.println();
-  //After combining the high byte and low byte, xA = xA/16384.0, xG = xG/131.0, and T = T/340.00 + 36.53
-  delay(100); // Wait for 100 milliseconds before the next reading
+
+
+  // // Pack roll and pitch into a byte buffer for sending data
+  uint8_t buffer[8]; // Buffer to hold packed roll and pitch data 8 bytes
+  packFloatToBytes(roll, buffer, 0);     // Pack roll at index 0
+  packFloatToBytes(pitch, buffer, 4);    // Pack pitch at index 4
+
+// Print the content of the buffer in binary format for debugging
+  Serial.print("Buffer content (binary): ");
+  for (int i = 0; i < 8; i++) {
+    Serial.print(buffer[i], BIN); // Print each byte in binary
+    Serial.print(" ");
+  }
+  Serial.println();
+  // packFloatToBytes(xAcc, buffer, 8);     // Pack xAcc at index 8
+  // packFloatToBytes(yAcc, buffer, 12);    // Pack yAcc at index 12
+  // packFloatToBytes(zAcc, buffer, 16);    // Pack zAcc at index 16
+  // packFloatToBytes(xGyr, buffer, 20);    // Pack xGyr at index 20
+  // packFloatToBytes(yGyr, buffer, 24);    // Pack yGyr at index 24
+  // packFloatToBytes(zGyr, buffer, 28);    // Pack zGyr at index 28
+  // packFloatToBytes(Temp, buffer, 32);    // Pack Temp at index 32
+
+  //UDP Sending data
+  udp.beginPacket(udpAddress, udpPort);
+  udp.write(buffer, 8); //send only 8 bytes
+  udp.endPacket();
+  delay(100);
+
+  memset(buffer, 0, 8); //clear buffer for the next cycle
+
+  udp.parsePacket();
+  if(udp.read(buffer, 1) > 0){ //checks if there is an incoming packet from the server
+    Serial.print("Server to client: ");
+    ack = buffer[0]; //If a packet is received, the first byte is stored in ack
+    Serial.println((String)ack);
+  }
+  
+  delay(10); /// Wait for 10 milliseconds before the next reading
+}
+
+// Function to pack a float into 4 bytes using bitwise operations
+// Uses '>': big-endian byte order, where MSB is stored first (lowest memory location)
+void packFloatToBytes(float value, uint8_t* buffer, int startIndex) {
+  uint32_t temp = *((uint32_t*)&value); // Cast float to uint32_t (32-bit integer) hen dereference to get the bits
+  buffer[startIndex] = (temp >> 24) & 0xFF; // Extract most significant byte
+  buffer[startIndex + 1] = (temp >> 16) & 0xFF; // Extract second byte
+  buffer[startIndex + 2] = (temp >> 8) & 0xFF; // Extract third byte
+  buffer[startIndex + 3] = temp & 0xFF; // Extract least significant byte
+}
+
+// Function to unpack 4 bytes from the buffer into a float
+float unpackBytesToFloat(uint8_t* buffer, int startIndex) {
+  uint32_t temp = 0;
+  temp |= (buffer[startIndex] << 24);
+  temp |= (buffer[startIndex + 1] << 16);
+  temp |= (buffer[startIndex + 2] << 8);
+  temp |= buffer[startIndex + 3];
+  
+  float value = *((float*)&temp); // Cast the 32-bit integer back to a float
+  return value;
 }
--- app1.py
+++ app1.py
@@ -8,6 +8,26 @@ from OpenGL.GL import * # used for rendering 3D graphics
 from OpenGL.GLU import *
 import sys # used to exit the program gracefully when a specific key is pressed
 
+from socket import *
+import numpy as np
+from time import time, strftime
+from numpy import frombuffer, array, savetxt
+from tkinter import Tk, Label, Button, scrolledtext
+from tkinter import INSERT, END
+from threading import Thread
+from functools import partial
+from datetime import datetime
+import struct
+
+# Set up UDP server
+UDP_IP = "0.0.0.0"  # Assigned IP address to socket. 0.0.0.0 is used as a way for the server to listen to all the interfaces of this PC.
+UDP_PORT = 6000 # Assigned port to socket. Port will be different
+sock = socket(AF_INET, SOCK_DGRAM) ## Create a socket object with IPv4 and UDP protocol
+sock.bind((UDP_IP, UDP_PORT)) #bind socket to the port and IP defined above
+print('server bind at ', sock) #print successful binding message
+BUFFER_SIZE = 8 # sets the size of the buffer that will be used to hold incoming data packets
+
+
 Vertices = ((1, 1, 1), (1, 1, -1), (1, -1, -1), (1, -1, 1), (-1, 1, 1), (-1, -1, -1), (-1, -1, 1), (-1, 1, -1)) # 3D coordinates of the cube's 8 corners. Each tuple represents the (x, y, z) coordinates of a vertex.
 Edges = ((0, 1), (0, 3), (0, 4), (1, 2), (1, 7), (2, 5), (2, 3), (3, 6), (4, 6), (4, 7), (5, 6), (5, 7)) # define the pairs of vertices that form the 12 edges of the cube.
 Quads = ((0, 3, 6, 4), (2, 5, 6, 3), (1, 2, 5, 7), (1, 0, 4, 7), (7, 4, 6, 5), (2, 3, 0, 1)) # define the 6 faces (quads) of the cube. Each face is represented by a tuple of four vertex indices.
@@ -19,34 +39,62 @@ def wireCube():
             glVertex3fv(Vertices[vertex])
     glEnd() #Ends the drawing operation for the current shape.
 
+def unpack_bytes_to_float(buffer, start_index):
+    # Extract 4 bytes from the buffer
+    byte_data = buffer[start_index:start_index + 4]  # Get the relevant 4 bytes
+    # Unpack the bytes into a float
+    value = struct.unpack('>f', byte_data)  # '>f' means big-endian float
+    return value[0]  # Return the first element of the tuple
 
 
+# Initialize Pygame and OpenGL
 pg.init() # initialize pygame modules
 display = (800, 600) # Sets the window size to 800x600 pixels
 pg.display.set_mode(display, DOUBLEBUF|OPENGL) # Create a display window using pygame. set to use double buffering with openGL rendering
 gluPerspective(45, (display[0]/display[1]), 0.1, 50.0) # Sets up the camera’s perspective. The field of view is 45 degrees, the aspect ratio is 800/600, and objects are visible between distances of 0.1 and 50 units from the camera.
 glTranslatef(0.0, 0.0, -5) # Moves the entire scene 5 units away from the camera, so the cube is visible within the perspective.
 
-while True:
-    for event in pg.event.get():
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_LEFT: # If the user presses the left arrow key the cube rotates 1 degree around the y-axis
-                glRotatef(1, 0, 1, 0)
-            if event.key == pg.K_RIGHT: # If the user presses the right arrow key the cube rotates in the opposite direction around the y-axis
-                glRotatef(-1, 0, 1, 0)
-            if event.key == pg.K_UP: # The up and down arrow keys rotate the cube around the x-axis
-                glRotatef(1, 1, 0, 0)
-            if event.key == pg.K_DOWN:
-                glRotatef(-1, 1, 0, 0)
-            if event.key == pg.K_z: # the z and x keys rotate it around the z-axis
-                glRotatef(1, 0, 0, 1)
-            if event.key == pg.K_x:
-                glRotatef(-1, 0, 0, 1)
-            if event.key == pg.K_q: # If the 'q' key is pressed, Pygame is quit, and the program exits.
-                pg.quit()
-                sys.exit()
 
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT) # Clears the color and depth buffers, ensuring a fresh frame is drawn on each iteration of the loop
+# Create a custom event that triggers every (10 ms)
+AUTO_ROTATE_EVENT = pg.USEREVENT + 1
+pg.time.set_timer(AUTO_ROTATE_EVENT, 10)  # Set the timer for 1-second intervals
+
+while True:
+    data, addr = sock.recvfrom(BUFFER_SIZE)
+    #if len(data) == 8:  # Ensure we've received 8 bytes (two floats for roll and pitch)
+    roll, pitch = struct.unpack('>ff', data)  # Unpack two floats. Converts the 8-byte data into two floats corresponding to roll and pitch.
+    # Unpack the same data as two unsigned 32-bit integers for binary representation
+    # roll_b = struct.unpack('I', data[:4])[0]  # First 4 bytes for roll
+    # pitch_b = struct.unpack('I', data[4:])[0]  # Next 4 bytes for pitch
+    print(f'Received roll: {roll}, pitch: {pitch}, from: {addr}\n')
+    # print(f'Binary roll: {format(roll_b, "032b")}\n')
+    # print(f'Binary pitch: {format(pitch_b, "032b")}\n')
+    
+    # Initial rotation
+    glRotatef(1, 1, 0, 0)
+    
+    for event in pg.event.get():
+       if event.type == AUTO_ROTATE_EVENT:
+           if np.abs(pitch) > np.abs(roll):
+               # Apply pitch rotation around the X-axis
+               if (pitch > 0):
+                   glRotatef(1, 0, 1, 0)
+               elif (pitch < 0):
+                   glRotatef(-1, 0, 1, 0)
+               # Apply roll rotation around the z-axis  
+           else:
+               if (roll > 0):
+                   glRotatef(1, 0, 0, 1)
+               elif(roll < 0):
+                   glRotatef(-1, 0, 0, 1)
+       if event.type == pg.KEYDOWN:
+           if event.key == pg.K_q: # If the 'q' key is pressed, Pygame is quit, and the program exits.
+               pg.quit()
+               sys.exit()
+               
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Clears the color and depth buffers, ensuring a fresh frame is drawn on each iteration of the loop and apply the orientation transformations
+  
+    
     wireCube() # draw the wireframe cube
     pg.display.flip() # Updates the display to show the newly drawn frame
-    pg.time.wait(10) # wait 10 milliseconds between frames to control the frame rate and make the rotations smoother
+    pg.time.wait(100) # wait 100 milliseconds between frames to control the frame rate and make the rotations smoother
